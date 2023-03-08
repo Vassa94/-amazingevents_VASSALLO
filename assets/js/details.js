f2f384e40ebef01ced9b08c0ad41f7f94d1d08f1
Vassa94 <vassallo1994gmail.com>
2023-03-07 21:11:31 -0300

    task3

--- assets/js/details.js
+++ assets/js/details.js
@@ -2,11 +2,12 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const cardData = JSON.parse(decodeURIComponent(urlParams.get('cardData')));
 
+console.log(cardData);
 
-console.log(cardData)
 
 const detailContainer = document.querySelector('#details');
-const cardElement = document.createElement('div');
+const details = document.createElement('div');
+details.classList.add('d-flex', 'justify-content-between', 'align-items-center', 'gap-3','p-3', 'm-2', 'border')
 details.innerHTML = `    
         <figure class="border w-50 h-100 sombra-photo">
             <img class="detail-photo p-2" src="${cardData.image}" alt="Photo">
@@ -18,19 +19,28 @@ details.innerHTML = `
             <p>
                 ${cardData.description}
             </p>
+            <p>
+                Date: ${cardData.date}
+            </p>
+            <p>
+               Capacity: ${cardData.capacity} 
+            </p>
+            <p>
+               Price:  $${cardData.price}
+            </p>
         </article>
     `;
-    detailContainer.appendChild(details);
+detailContainer.appendChild(details);
 
-/*  */
-
-/*  <div class="card m-2" style="width: 18rem;">
-    <img src="${cards.events[i].image}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">${cards.events[i].name}</h5>
-        <p class="card-text">${cards.events[i].description}</p>
-    </div>
-    <div class="card-footer d-flex justify-content-between align-items-center ">
-        <a href="">Precios: $${cards.events[i].price}</a>
-        <a href="/pages/details.html?cardData=${encodeURIComponent(JSON.stringify(card))}" class="btn btn-ae border">Ver más...</a>                </div>
-</div> */
+/* {
+    "id": 3,
+    "image": "https://i.postimg.cc/GmHRkbNV/Jurassic-Park.jpg",
+    "name": "Jurassic Park",
+    "date": "2021-11-02",
+    "description": "Let's go meet the biggest dinosaurs in the paleontology museum.",
+    "category": "Museum",
+    "place": "Field",
+    "capacity": 82000,
+    "assistance": 65892,
+    "price": 15
+} */
--- assets/js/past_events.js
+++ assets/js/past_events.js
@@ -1,63 +1,159 @@
-fetch("/assets/data.json")
-    .then(response => response.json())
-    .then(data => {
+let cards = [];
+const $checks = document.getElementById('category-section');
+const $search = document.querySelector('input[placeholder="Search"]');
 
-        const cards = data;
+function dataFetch() {
+    fetch("/assets/data.json")
+        .then(response => response.json())
+        .then(data => {
+            cards = data;
+            displayCards(cards.events)
+            carrousel(cards.events);
+            displayCategory(cards.events);
+        })
+        .catch(error => console.error(error));
+}
 
-        const cardFragment = document.createDocumentFragment();
-        for (let i = 0; i < cards.events.length; i++) {
-            if (cards.events[i].date < cards.currentDate) {
-                const cardElement = document.createElement('div');
-                cardElement.classList.add('card', 'm-2');
-                cardElement.style.width = '18rem';
-                cardElement.innerHTML = `
-                        <img src="${cards.events[i].image}" class="card-img-top" alt="${cards.events[i].name}">
-                        <div class="card-body">
-                            <h5 class="card-title">${cards.events[i].name}</h5>
-                            <p class="card-text">${cards.events[i].description}</p>
-                        </div>
-                        <div class="card-footer d-flex justify-content-between align-items-center ">
-                            <a href="">Precios: $${cards.events[i].price}</a>
-                            <a href="/pages/details.html?cardData=${encodeURIComponent(JSON.stringify(cards.events[i]))}" class="btn btn-ae border">Ver más...</a>
-                        </div>`;
+dataFetch();
 
-                cardFragment.appendChild(cardElement);
-            }
+function displayCards(array) {
+    const cardContainer = document.getElementById('card-container');
+    const noResults = document.getElementById('no-results');
+    cardContainer.innerHTML = ''; 
+
+    const cardFragment = document.createDocumentFragment();
+    array.forEach((element) => {
+        if (element.date < cards.currentDate) {
+            const cardElement = document.createElement('div');
+            cardElement.classList.add('card', 'm-2');
+            cardElement.style.width = '18rem';
+            cardElement.innerHTML = `
+                <img src="${element.image}" class="card-img-top" alt="${element.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${element.name}</h5>
+                    <p class="card-text">${element.description}</p>
+                </div>
+                <div class="card-footer d-flex justify-content-between align-items-center ">
+                    <a href="">Precios: $${element.price}</a>
+                    <a href="/pages/details.html?cardData=${encodeURIComponent(JSON.stringify(element))}" class="btn btn-ae border">Ver más...</a>
+                </div>`;
+            cardFragment.appendChild(cardElement);
         }
+    });
 
-        const cardContainer = document.getElementById('card-container');
-        cardContainer.appendChild(cardFragment);
+    cardContainer.appendChild(cardFragment);
 
-        $(document).ready(function () {
-            $('#card-container').slick({
-                infinite: true,
-                slidesToShow: 6,
-                slidesToScroll: 1,
-                autoplay: true,
-                autoplaySpeed: 2000,
-                responsive: [
-                    {
-                        breakpoint: 1460,
-                        settings: {
-                            slidesToShow: 4
-                        }
-                    },
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 2
-                        }
-                    },
-                    {
-                        breakpoint: 576,
-                        settings: {
-                            slidesToShow: 1
-                        }
-                    }
-                ],
-                prevArrow: $('#slider-controls .btn-prev'),
-                nextArrow: $('#slider-controls .btn-next')
-            });
-        });
-    })
-    .catch(error => console.error(error));
+    if (array.length === 0) {
+        noResults.classList.remove('d-none'); 
+    } else {
+        noResults.classList.add('d-none');
+    }
+
+
+
+}
+
+const carrousel = (array) => {
+    let slidesToShow = 5;
+    if (array.length <= 5) { 
+        slidesToShow = array.length; 
+    }
+    $('#card-container').slick({
+        infinite: true,
+        centerMode: array.length > 5,
+        focusOnSelect: true,
+        slidesToShow: slidesToShow,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1460,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ],
+        prevArrow: $('#slider-controls .btn-prev'),
+        nextArrow: $('#slider-controls .btn-next')
+    });
+}
+
+const catArray = (array) => {
+    const categoriesSet = new Set();
+    array.forEach((event) => categoriesSet.add(event.category));
+    return Array.from(categoriesSet);
+}
+
+function displayCategory(array) {
+    let category = catArray(array);
+    const catFragment = document.createDocumentFragment();
+    category.forEach((element) => {
+        const catElement = document.createElement('label');
+        catElement.classList.add('form-check-label');
+        catElement.innerHTML = `
+            <input type="checkbox" id="${element.toLowerCase()}" class="form-check-input" name="${element}">
+            ${element}`;
+        catFragment.appendChild(catElement);
+    });
+    const catContainer = document.getElementById('category-section');
+    catContainer.appendChild(catFragment);
+}
+
+const searchFilter = (array, search) => {
+    let filteredArray = array.filter(element => element.name.toLowerCase().includes(search.toLowerCase()))
+    return filteredArray
+}
+
+const categoryFilter = (array) => {
+    let checked = document.querySelectorAll('input[type="checkbox"]:checked');
+    let categories = Array.from(checked).map(el => el.id.toLowerCase());
+    let filteredArray = array.filter(element => categories.some(category => element.category.toLowerCase().includes(category)));
+    return filteredArray;
+}
+
+const unifiedFilter = (array) => {
+    let filteredArray = searchFilter(array, $search.value);
+    let checkedCategories = document.querySelectorAll('input[type="checkbox"]:checked');
+    if (checkedCategories.length > 0) {
+        filteredArray = categoryFilter(filteredArray);
+    }
+    return filteredArray;
+};
+
+$checks.addEventListener('change', () => {
+    let dataFilter = unifiedFilter(cards.events)
+    console.log(dataFilter)
+    $('#card-container').slick('unslick');
+    displayCards(dataFilter)
+    carrousel(dataFilter);
+
+})
+
+$search.addEventListener('input', (e) => {
+    let dataFilter = unifiedFilter(cards.events)
+    $('#card-container').slick('unslick');
+    displayCards(dataFilter);
+    carrousel(dataFilter);
+})
+
+
+
+
+
+
+
+
+
